589a5edecc7df0a415c48ec3a5ec85b262b69162
Tomomi-K1 <tomomi.music@gmail.com>
2023-03-20 15:07:30 -0700

    finished first task of adding unit test for sqlForPartialUpdate function in helpers/sql.js

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,10 +1,11 @@
 const { sqlForPartialUpdate}  = require("./sql");
+const {BadRequestError} = require('../expressError')
 const data = {
   "password":"test123",
   "firstName": "Test",
   "lastName" : "Tesko",
   "email": "test@gmail.com",
-  "isAdmn" : false
+  "isAdmin" : false
 }
 
 const jsToSql={
@@ -21,15 +22,15 @@ describe("Checking partial UPdate function works", function () {
     const {setCols, values} = sqlForPartialUpdate(data, jsToSql);
     expect(values).toHaveLength(5);
     expect(values).toContain('test@gmail.com');
-    expect(setCols).toEqual("password=$1, first_name=$2, last_name=$3, email=$4, is_admin=$5");
+    expect(setCols).toEqual('"password"=$1, "first_name"=$2, "last_name"=$3, "email"=$4, "is_admin"=$5');
     });
 
 
   test("Received data for update is empty: admin", function () {
     try{
-    const {setCols, values} = sqlForPartialUpdate(data, jsToSql);
-    } catch(e){
-      expect(e).toEqual(expect.stringContaining('No data'));
+    const {setCols, values} = sqlForPartialUpdate({}, jsToSql);
+    } catch(err){
+      expect(err instanceof BadRequestError).toBeTruthy();
     } 
   });
 
